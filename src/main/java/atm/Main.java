493fcd8f2f7524a9e63ebbafdb28abf4296b7497
Natdadai <natdadai.t@ku.th>
2022-07-24 22:09:18 +0700

    annotation

---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,14 @@
+package atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext app
+                = new ClassPathXmlApplicationContext("bean.xml");
+        AtmUI atmUI = app.getBean(AtmUI.class);
+        atmUI.run();
+    }
+}
